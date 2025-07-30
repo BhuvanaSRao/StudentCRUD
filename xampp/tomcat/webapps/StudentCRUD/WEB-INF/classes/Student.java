d84d7132b375c77254f1a08b5b24a85735605f7a
BhuvanaSRao <bhuvanasrao2@gmail.com>
2025-07-30 15:08:14 +0530

    Create Student.java
    This is my Model (where properties are defined)

---
+++ xampp/tomcat/webapps/StudentCRUD/WEB-INF/classes/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private int rollNo;
+    private String name;
+    private String className;
+    private int mark1;
+    private int mark2;
+
+
+    public int getRollNo() { return rollNo; }
+    public void setRollNo(int rollNo) { this.rollNo = rollNo; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getClassName() { return className; }
+    public void setClassName(String className) { this.className = className; }
+
+    public int getMark1() { return mark1; }
+    public void setMark1(int mark1) { this.mark1 = mark1; }
+
+    public int getMark2() { return mark2; }
+    public void setMark2(int mark2) { this.mark2 = mark2; }
+}
